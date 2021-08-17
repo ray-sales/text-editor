a505064bd26c5fa77e3bc505d75ad8d799ca6248
Ray Sales <78052891+ray-sales@users.noreply.github.com>
2021-08-17 09:56:49 -0300

    initialize edit method

--- Program.cs
+++ Program.cs
@@ -6,7 +6,41 @@ namespace text_editor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Menu();
+        }
+
+        static void Menu(){
+            Console.Clear();
+            Console.WriteLine("What do You want do?");
+            Console.WriteLine("1 - Open file");
+            Console.WriteLine("2 - New File");
+            Console.WriteLine("0 - Exit");
+
+            short optionSelected = short.Parse(Console.ReadLine());
+
+            switch(optionSelected){
+                case 0: System.Environment.Exit(0); break;
+                case 1: Open(); break;
+                case 2: Edit();break;
+                default: Menu(); break;
+            }
+        }
+
+        static void Open(){
+
+        }
+
+        static void Edit(){
+            Console.Clear();
+            Console.WriteLine("Digite your text bellow (ESC for exit)");
+            Console.WriteLine("------------------");
+
+            string text = "";
+
+            while(Console.ReadKey().Key != ConsoleKey.Escape){
+                
+            }
+            
         }
     }
 }
